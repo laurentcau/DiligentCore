df6acee14bec1d36a9d36e14922e6ccc81eaf626
Laurent Caumont <laurent.caumont@bentley.com>
2020-05-05 09:42:41 +0200

    Add adapter capability flags

--- Graphics/GraphicsEngineD3DBase/include/EngineFactoryD3DBase.hpp
+++ Graphics/GraphicsEngineD3DBase/include/EngineFactoryD3DBase.hpp
@@ -47,6 +47,17 @@ public:
     {}
 
 
+    // Returns bool whether the device supports DirectX Raytracing tier.
+	bool IsDirectXRaytracingSupported(IDXGIAdapter1* adapter)
+	{
+		CComPtr<ID3D12Device> testDevice;
+		D3D12_FEATURE_DATA_D3D12_OPTIONS5 featureSupportData = {};
+
+		return SUCCEEDED(D3D12CreateDevice(adapter, D3D_FEATURE_LEVEL_12_0, IID_PPV_ARGS(&testDevice)))
+			&& SUCCEEDED(testDevice->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS5, &featureSupportData, sizeof(featureSupportData)))
+			&& featureSupportData.RaytracingTier != D3D12_RAYTRACING_TIER_NOT_SUPPORTED;
+	}
+
     virtual void DILIGENT_CALL_TYPE EnumerateAdapters(DIRECT3D_FEATURE_LEVEL MinFeatureLevel,
                                                       Uint32&                NumAdapters,
                                                       GraphicsAdapterInfo*   Adapters) override
@@ -67,6 +78,9 @@ public:
                 auto& Attribs = Adapters[adapter];
 
                 Attribs = DXGI_ADAPTER_DESC_To_GraphicsAdapterInfo(AdapterDesc);
+
+                if (IsDirectXRaytracingSupported(pDXIAdapter))
+                    Attribs.CapabilityFlags = Attribs.CapabilityFlags | ADAPTER_CAPABILITY_RAYTRACING;
 
                 Attribs.NumOutputs = 0;
                 CComPtr<IDXGIOutput> pOutput;
