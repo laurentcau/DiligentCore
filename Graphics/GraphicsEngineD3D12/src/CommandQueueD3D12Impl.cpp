98c0de78bf3c53c75088351eadd6824b4d757fb6
Laurent Caumont <laurent.caumont@bentley.com>
2020-08-20 11:12:43 +0200

    Log fatal error on wrong Fence values

--- Graphics/GraphicsEngineD3D12/src/CommandQueueD3D12Impl.cpp
+++ Graphics/GraphicsEngineD3D12/src/CommandQueueD3D12Impl.cpp
@@ -102,6 +102,11 @@ Uint64 CommandQueueD3D12Impl::GetCompletedFenceValue()
     auto CompletedFenceValue = m_d3d12Fence->GetCompletedValue();
     VERIFY(CompletedFenceValue != UINT64_MAX, "If the device has been removed, the return value will be UINT64_MAX");
 
+    if (CompletedFenceValue == UINT64_MAX) // see https://docs.microsoft.com/en-us/windows/win32/api/d3d12/nf-d3d12-id3d12fence-getcompletedvalue
+    {
+        LOG_FATAL_ERROR_MESSAGE("Device removed");
+    }
+
     if (CompletedFenceValue > m_LastCompletedFenceValue)
         m_LastCompletedFenceValue = CompletedFenceValue;
     return m_LastCompletedFenceValue;
